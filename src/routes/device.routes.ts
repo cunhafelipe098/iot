e9f4a690ed18ea2daca2fec2062111e687a27275
cunhafelipe098 <cunhafelipe098@gmail.com>
2023-02-25 16:52:35 -0300

    feat: create link with createDeviceService

--- src/routes/device.routes.ts
+++ src/routes/device.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from "express"
+import { CreateDeviceService } from "../services/CreateDeviceService"
+import { DevicesRepository } from "../repositories/DevicesRepository"
 
 const deviceRoutes = Router();
+const devicesRepository = new DevicesRepository()
 
-deviceRoutes.get("/", (request, response) => {
-  return response.json({
-    menssage: "get device"
-  });
+deviceRoutes.post("/", (request, response) => {
+  const { device_id, name, nickname } = request.body;
+
+  const createDeviceService = new CreateDeviceService(devicesRepository);
+  
+  createDeviceService.execute({ device_id, name, nickname });
+
+  return response.status(201).send();
 });
 
 export { deviceRoutes };
