ea25879b4c968e850419790b838bf46331de328e
cunhafelipe098 <cunhafelipe098@gmail.com>
2023-02-25 12:46:42 -0300

    feat: create service of device

---
+++ src/services/CreateDeviceService.ts
@@ -0,0 +1,51 @@
+import { Device } from "../schemas/Device";
+import "reflect-metadata";
+import { injectable } from "tsyringe";
+import { DevicesRepository } from "../repositories/DevicesRepository";
+
+interface createUserDTO {
+  device_id: string,
+  name: string,
+  nickname: string
+}
+
+@injectable()
+class CreateDeviceService {
+  async execute({ device_id, name, nickname}: createUserDTO) {
+    const repository = new DevicesRepository();
+    
+    
+    const deviceAlreadyExists = await Device.findOne({
+      device_id,
+    }).exec();
+
+    if (deviceAlreadyExists) {
+
+      const device = await Device.findOneAndUpdate(
+        {
+          _id: deviceAlreadyExists._id,
+        },
+        {
+          $set: {name, nickname},
+        },
+        {
+          new: true,
+        }
+      );
+      return device;
+    } else {
+
+      
+      
+      const device = await repository.create({
+        device_id,
+        name,
+        nickname
+      });
+      
+      return device;
+    }
+  }
+}
+
+export { CreateDeviceService };
